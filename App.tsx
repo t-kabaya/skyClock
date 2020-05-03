a3d368694737559dc9fc4832ba0c2b1ba9952856
kaba <taikikabaya@gmail.com>
2020-05-03 23:18:55 +0900

    アプリを回転可能に。App.tsxをリファクタリング

--- App.tsx
+++ App.tsx
@@ -17,8 +17,8 @@ import grads from './skyColor'
 
 const { height, width } = Dimensions.get('window')
 const maxWidth = height > width ? height : width
-// 回転を止めて欲しい？
-// ScreenOrientation.allow(ScreenOrientation.Orientation.ALL)
+
+ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.ALL)
 
 UIManager.setLayoutAnimationEnabledExperimental &&
   UIManager.setLayoutAnimationEnabledExperimental(true)
@@ -26,73 +26,65 @@ UIManager.setLayoutAnimationEnabledExperimental &&
 const interval = __DEV__ ? 1000 : 1
 
 export default () => {
-  useKeepAwake()
-
   const [calculating, setCalculating] = useState(true)
-  const [hour, setHour] = useState(0)
-  const [minute, setMinute] = useState('')
-  const [backgroundSkyColor, setBackgroundSkyColor] = useState(grads[0])
+  const [hour, setHour] = useState('00')
+  const [minute, setMinute] = useState('00')
+  const [backgroundColor, setBackgroundColor] = useState(grads[0])
+  
+  useKeepAwake()
   const moment = Moment()
 
   useEffect(() => {
     setInterval(() => {
-      const hour = parseInt(moment.format('H'))
-      const minute =
+      const hour: string = moment.format('H')
+      const minute: string =
         moment.format('m').length === 1
           ? '0' + moment.format('m')
           : moment.format('m')
+
       LayoutAnimation.easeInEaseOut()
+
       moment.add(interval, 'seconds')
+
       setHour(hour)
       setMinute(minute)
+      setBackgroundColor(grads[parseInt(hour) * 4 + Math.floor(parseInt(minute) / 15)])
       setCalculating(false)
-      setBackgroundSkyColor(grads[hour * 4 + Math.floor(minute / 15)])
     }, 1000)
 
-    Font.loadAsync({
-      'text-me-one': require('./TextMeOne-Regular.ttf')
-    })
+    Font.loadAsync({'text-me-one': require('./TextMeOne-Regular.ttf')})
   },[])
 
-    if (calculating) return null
-    const firstNum = hour.toString()[hour.toString().length - 2]
-    const secondNum = hour.toString()[hour.toString().length - 1]
-    const thirdNum = minute.toString()[0]
-    const forthNum = minute.toString()[1]
-    return (
-      <View style={S.container}>
-        <AnimatedGradientTransition
-          colors={backgroundSkyColor}
-          style={{
-            padding: 15,
-            alignItems: 'center',
-            borderRadius: 5,
-            width: maxWidth,
-            height: height
-          }}
-        >
-          <View style={S.gradientContainer}>
-            <StatusBar style={{ backgroundColor: 'transparent' }} />
-            <Text style={[S.timeText, { paddingRight: 260 }]}>
-              {firstNum}
-            </Text>
-            <Text style={[S.timeText, { paddingRight: 110 }]}>
-              {secondNum}
-            </Text>
-            <Text style={S.timeText}>:</Text>
-            <Text style={[S.timeText, { paddingLeft: 110 }]}>
-              {thirdNum}
-            </Text>
-            <Text style={[S.timeText, { paddingLeft: 260 }]}>
-              {forthNum}
-            </Text>
-          </View>
-        </AnimatedGradientTransition>
-
-        {/* <KeepAwake /> */}
-      </View>
-    )
-  
+  if (calculating) return null
+  const firstNum = hour[hour.length - 2]
+  const secondNum = hour[hour.length - 1]
+  const thirdNum = minute[0]
+  const forthNum = minute[1]
+  return (
+    <View style={S.container}>
+      <AnimatedGradientTransition
+        colors={backgroundColor}
+        style={S.animatedGradientTransition}
+      >
+        <View style={S.gradientContainer}>
+          <StatusBar style={{ backgroundColor: 'transparent' }} />
+          <Text style={[S.timeText, { paddingRight: 260 }]}>
+            {firstNum}
+          </Text>
+          <Text style={[S.timeText, { paddingRight: 110 }]}>
+            {secondNum}
+          </Text>
+          <Text style={S.timeText}>:</Text>
+          <Text style={[S.timeText, { paddingLeft: 110 }]}>
+            {thirdNum}
+          </Text>
+          <Text style={[S.timeText, { paddingLeft: 260 }]}>
+            {forthNum}
+          </Text>
+        </View>
+      </AnimatedGradientTransition>
+    </View>
+  )
 }
 
 const S = StyleSheet.create({
@@ -112,5 +104,12 @@ const S = StyleSheet.create({
     fontSize: 130,
     position: 'absolute',
     fontFamily: 'text-me-one'
+  },
+  animatedGradientTransition: {
+    padding: 15,
+    alignItems: 'center',
+    borderRadius: 5,
+    width: maxWidth,
+    height: height
   }
 })
